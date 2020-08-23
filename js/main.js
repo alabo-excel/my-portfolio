e1f135a82db3b31b73f97da3b9e2f9a86a4aa7bd
alabo-excel <iamalaboexcel@gmail.com>
2020-08-23 16:07:04 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// var toggle = document.getElementById('toggle')
+// var main = document.getElementsByTagName('body')
+
+// toggle.addEventListener('click', () => {
+//     main.css.backgroundColor = 'white'
+
+// })
